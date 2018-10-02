6296be1f1d2c4d4d2b872265fc1353fc2b27a74f
Dave Augustus <davetweetlive@gmail.com>
2018-10-03 00:25:53 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,36 +1,89 @@
 let budgetController = (function(){
     
-    //Some code
-
+    let Expense = function(id, description, value){
+                    this.id          = id;
+                    this.description = description;
+                    this.value       = value;
+                };
+    let Income  = function(id, description, value){
+                    this.id          = id;
+                    this.description = description;
+                    this.value       = value;
+                };
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 })();
+
+
+
 
 
 let UIController = (function(){
 
-    //some code
-
+    let DOMStrings = {
+                         inputType: '.add__type',
+                         inputDesc: '.add__description',
+                         inputAmmount: '.add__value',
+                         inputButton: '.add__btn'
+                     } 
+    return {
+        getInput: function(){
+                    return {
+                        type:           document.querySelector(DOMStrings.inputType).value,
+                        description:    document.querySelector(DOMStrings.inputDesc).value,
+                        ammount:        document.querySelector(DOMStrings.inputAmmount).value
+                    };
+                 },
+        getDOMStrings: function(){
+                        return DOMStrings;
+                     }
+    };
+    
 })();
 
 
 let controller = (function(budgetCtrl, UICtrl){
 
+    let setUpEventListeners = function(){
+                                let DOM = UICtrl.getDOMStrings();
+
+                                document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+                                document.addEventListener('keypress', function(){
+                                    if(event.key == 'Enter'){
+                                        ctrlAddItem();
+                                    }
+                                });
+
+                            }
     let ctrlAddItem = function(){
-        // 1. Get input data
-        // 2. Add into the budget controller
-        // 3. Add into the UI
-        // 4. Calculate the budget 
-        // 5. Dislay budget on the UI
-        console.log('It\'s working fine');
-    }    
 
-   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+                        // 1. Get input data from UICtrl as dictionary 
+                        let inputData = UICtrl.getInput();
+                        console.log(inputData);
 
-   document.addEventListener('keypress', function(){
+                        // 2. Add into the budget controller
+                        // 3. Add into the UI
+                        // 4. Calculate the budget 
+                        // 5. Dislay budget on the UI
+                        
+                    }    
+    return {
+                init: function(){
+                    console.log('Application has Started!');
+                    setUpEventListeners();
+                }
+            }
 
-        if(event.key == 'Enter'){
-            ctrlAddItem();
-            console.log(event);
-        }
-   });
 
 })(budgetController, UIController);
+
+controller.init();
